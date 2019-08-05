8aa5900cd3709e3cadb0391752fb3fc0d17c8994
Steve Holden <steve@holdenweb.com>
2019-08-05 13:27:02 +0100

    Improve comments.

--- src/beautifulsoup/scrape_loop.py
+++ src/beautifulsoup/scrape_loop.py
@@ -1,16 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
-
+#
 # Store the basic URL we need
+#
 url = "https://beta.charitycommission.gov.uk/charity-details/"
+
+#
 # Fetch the URL, also adding some parameters at the end. The resulting URL is
 # https://beta.charitycommission.gov.uk/charity-details/?regId=213890
 # Create a list of charity IDs:
+#
 charityids = ["213890", "213891", "213892"]
+
+#
 # Loop through them:
+#
 for eachid in charityids:
+    # Get the page's response, whose `content` attribute holds the HTML
     response = requests.get(url, params={"regId": eachid})
-    # Now drill down into the page ('response'), and turn it into a more easily scrapable object
+    # Now turn the HTML into a more easily scrapable object
     soup = BeautifulSoup(response.content, "lxml")
     # Print the results of drilling down into that, specifying a particular attribute we want to grab
     print(soup.find("p", attrs={"class": "pcg-charity-details__amount"}))
