ac70f4d20794117866083a476c9912c01705f614
Steve Holden <steve@holdenweb.com>
2019-08-05 12:49:01 +0100

    Reformat code with black for standardised layout

--- src/beautifulsoup/scrape_demo.py
+++ src/beautifulsoup/scrape_demo.py
@@ -2,8 +2,8 @@
 # Import the libraries you need ... you may need to install them
 # into your Python environment as they don't come with Python.
 #
-import requests                      # For fetching webpages
-from bs4 import BeautifulSoup        # For drilling down into the webpage
+import requests  # For fetching webpages
+from bs4 import BeautifulSoup  # For drilling down into the webpage
 
 #
 # Store the basic URL we need
@@ -17,12 +17,12 @@ url = "https://beta.charitycommission.gov.uk/charity-details/"
 #
 #     https://beta.charitycommission.gov.uk/charity-details/?regId=213890
 #
-response = requests.get(url, params={'regId': "213890"})
+response = requests.get(url, params={"regId": "213890"})
 
 #
 # Now drill down into the page ('response'), and turn it into a more easily scrapable object
 #
-soup = BeautifulSoup(response.content, 'lxml')
+soup = BeautifulSoup(response.content, "lxml")
 
 #
 # Print the results of drilling down into that, specifying a particular attribute we want to grab
