63ca9bcb71ab5245f36af6411b0c57abedeeb48d
Steve Holden <steve@holdenweb.com>
2019-08-08 14:19:03 +0100

    Re-format scrape_loop for readability.

--- src/beautifulsoup/scrape_loop.py
+++ src/beautifulsoup/scrape_loop.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+
 #
 # Store the basic URL we need
 #
@@ -16,11 +17,22 @@ charityids = ["213890", "213891", "213892"]
 # Loop through them:
 #
 for eachid in charityids:
+    #
     # Get the page's response, whose `content` attribute holds the HTML
+    #
     response = requests.get(url, params={"regId": eachid})
+
+    #
     # Now turn the HTML into a more easily scrapable object
+    #
     soup = BeautifulSoup(response.content, "lxml")
+
+    #
     # Print the results of drilling down into that, specifying a particular attribute we want to grab
+    #
     print(soup.find("p", attrs={"class": "pcg-charity-details__amount"}))
+
+    #
     # Then showing the text only (no tags) using .text
+    #
     print(soup.find("p", attrs={"class": "pcg-charity-details__amount"}).text)
